fc22dc83d97ee3ab572bcc9552efba3b5c826cc0
Mari <mariya-antolevna@bk.ru>
2023-03-12 19:58:13 +0300

    Задача под номером2

---
+++ Task002/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine("Введите первое число");
+int a = Convert.ToInt32(  Console.ReadLine()  );
+
+Console.WriteLine("Введите второе число");
+int b = Convert.ToInt32(  Console.ReadLine()  );
+
+int max = a;
+int min = b;
+
+if (a > b) max = a;
+if (b > a) max = b;
+if (a < b) min = a;
+if (b < a) min = b;
+
+{
+    Console.Write("max = ");
+    Console.WriteLine(max);
+    Console.Write("min = ");
+    Console.WriteLine(min);
+}
